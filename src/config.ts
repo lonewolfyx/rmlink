badf23690ffbe3b4ea5140e41e708db0216f0a11
lonewolfyx <877085893@qq.com>
2026-02-06 20:24:12 +0800

    feat(config): add symLinkPath property to config object in resolveConfig

--- src/config.ts
+++ src/config.ts
@@ -5,6 +5,7 @@ import process from 'node:process'
 export const resolveConfig = (symLinkPath: string): IConfig => {
     return {
         cwd: process.cwd(),
+        symLinkPath,
         globalRoot: process.platform === 'win32' ? join(symLinkPath, 'node_modules') : join(symLinkPath, 'lib', 'node_modules'),
     }
 }
--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,6 @@
 export interface IConfig {
     cwd: string
+    symLinkPath: string
     globalRoot: string
 }
 
