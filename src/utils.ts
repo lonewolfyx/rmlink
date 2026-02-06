cc53dee8df782cc3084953b75f1e49ba30a6c6e2
lonewolfyx <877085893@qq.com>
2026-02-06 17:12:46 +0800

    feat(utils): add utility functions for symlink project detection

---
+++ src/utils.ts
@@ -0,0 +1,57 @@
+import type { IConfig, ISymLinkProjects } from '@/types.ts'
+import { lstat, readdir, readFile, readlink } from 'node:fs/promises'
+import { dirname, isAbsolute, join, resolve } from 'node:path'
+import { x } from 'tinyexec'
+
+export const getGlobalPrefix = async (): Promise<string> => {
+    const { stdout } = await x('npm', ['config', 'get', 'prefix'], {
+        nodeOptions: {
+            cwd: process.cwd(),
+        },
+    })
+    return stdout.trim()
+}
+
+export const isSymlink = async (p: string): Promise<boolean> => {
+    return (await lstat(p)).isSymbolicLink()
+}
+
+export const getSymLinkList = async (config: IConfig): Promise<ISymLinkProjects[]> => {
+    const symlinks: ISymLinkProjects[] = []
+
+    async function scan(dir: string, scope = '') {
+        const files = await readdir(dir)
+
+        for (const file of files) {
+            const fullPath = join(dir, file)
+            const stat = await lstat(fullPath)
+
+            if (stat.isSymbolicLink()) {
+                const pkgName = scope ? `${scope}/${file}` : file
+
+                let realPath: string = ''
+
+                realPath = await readlink(fullPath)
+                if (!isAbsolute(realPath)) {
+                    realPath = resolve(dirname(fullPath), realPath)
+                }
+
+                const pkgJsonPath = join(realPath, 'package.json')
+                const pkgContent = JSON.parse(await readFile(pkgJsonPath, 'utf-8'))
+
+                symlinks.push({
+                    name: pkgName,
+                    path: fullPath,
+                    realPath,
+                    bin: (typeof pkgContent.bin === 'string') ? [pkgContent.name] : Object.keys(pkgContent.bin),
+                })
+            }
+            else if (stat.isDirectory() && file.startsWith('@')) {
+                await scan(fullPath, file)
+            }
+        }
+    }
+
+    await scan(config.globalRoot)
+    return symlinks
+}
